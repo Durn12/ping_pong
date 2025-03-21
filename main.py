c7da9ccdf9fa4f4bf87140d371366809cf47dcc9
Durn12 <dimbug5498@gmail.com>
2025-03-21 20:07:48 +0300

    добавил класс GameSprite и Player

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
 import pygame
+
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load(player_image), (width, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+        
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+        
+class Player(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height, points):
+        super().__init__(player_image, player_x, player_y, player_speed, width, height)
+        self.points = points
+        
+    def update_r(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+            if self.rect.y > 550:
+                self.rect.y - 550
+                
+    def update_l(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+            if self.rect.y > 550:
+                self.rect.y - 550
